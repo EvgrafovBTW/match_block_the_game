55408d38da109777280f6eab557b7e14bed2c705
EvgrafovBTW <bleedthemwhite@yandex.ru>
2022-07-11 21:00:57 +0700

    initial

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  final String title = 'Match Blocks. THE GAME';
+
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: title,
+        theme: ThemeData(
+          scaffoldBackgroundColor: Color.fromARGB(255, 255, 251, 240),
+          primaryColor: Colors.red,
+        ),
+        home: MainMenu(),
+      );
+}
+
+class MainMenu extends StatefulWidget {
+  const MainMenu({Key? key}) : super(key: key);
+
+  @override
+  State<MainMenu> createState() => _MainMenuState();
+}
+
+class _MainMenuState extends State<MainMenu> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
